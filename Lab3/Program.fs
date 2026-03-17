6f8fb133b133019f7e398308f3a49e733ff1b6d1
Azecks <mrazecks@gmail.com>
2026-03-17 08:43:25 +0500

    Update Program.fs

--- Lab3/Program.fs
+++ Lab3/Program.fs
@@ -1,25 +1,22 @@
 ﻿open System
 open System.IO
 
-// Задание 1. Seq.map - последние цифры чисел
+
 let getLastDigits numbers =
     Seq.map (fun x -> 
         let xAsString = string x
         let lastCharacter = xAsString.[xAsString.Length - 1]
-        // Проверяем, является ли последний символ цифрой
         int (string lastCharacter)) numbers
 
-// Задание 2. Seq.fold - строка из списка символов
 let charsToString characters =
     Seq.fold (fun acc ch -> acc + string ch) "" characters
-
-// Задание 3. Функция для проверки наличия файлов в каталоге
+    
 let hasFilesInDirectory directoryPath =
-    let files = Directory.EnumerateFiles(directoryPath)  // получаем последовательность файлов
-    let isEmpty = Seq.isEmpty files                       // проверяем, пустая ли она
-    not isEmpty                                            // инвертируем результат
+    let files = Directory.EnumerateFiles(directoryPath)  
+    let isEmpty = Seq.isEmpty files                     
+    not isEmpty                                         
 
-// Функция для ввода последовательности вещественных чисел
+
 let readFloatSeq () =
     printf "Задание 1. Введите вещественные числа через пробел: "
     let input = Console.ReadLine()
@@ -31,7 +28,7 @@ let readFloatSeq () =
                 yield float s
     }
 
-// Функция для ввода последовательности символов
+
 let readCharSeq () =
     printf "Задание 2. Введите символы через пробел: "
     let input = Console.ReadLine()
@@ -48,20 +45,19 @@ let main argv =
    
     printfn "Ввод исходных данных"
     
-    // Вызов функций для ввода последовательностей
+
     let numbers = readFloatSeq ()
     let charSeq = readCharSeq ()
     
     printfn "\nРезультаты"
     
-    // Задание 1
+    
     printfn "Задание 1: Последние цифры чисел"
     printfn "  Исходная последовательность чисел: %A" (Seq.toList numbers)
     let lastDigits = getLastDigits numbers
     printfn "  Последние цифры: %A" (Seq.toList lastDigits)
     printfn ""
-    
-    // Задание 2
+
     printfn "Задание 2: Строка из последовательности символов"
     printfn "  Исходная последовательность символов: %A" (Seq.toList charSeq)
     let resultString = charsToString charSeq
@@ -69,14 +65,13 @@ let main argv =
 
     printfn "Задание 3: Проверка наличия файлов в каталоге"
     
-    // Ввод пути к каталогу
+
     printf "Введите путь к каталогу: "
     let directoryPath = Console.ReadLine()
     
-    // Проверяем наличие файлов
+
     let hasFiles = hasFilesInDirectory directoryPath
-    
-    // Выводим результат
+
     if hasFiles then
         printfn "В каталоге '%s' есть файлы" directoryPath
     else
